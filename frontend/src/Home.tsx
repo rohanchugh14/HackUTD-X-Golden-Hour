a1cbd2371113799877e3efe221fce673ae1f5dcb
Rohan Chugh <rohanchugh14@gmail.com>
2023-11-04 18:00:47 -0500

    more data cleaning and fe page setup
    Co-authored-by: Alexander N. Chin <Alexander-N-Chin@users.noreply.github.com>

---
+++ frontend/src/Home.tsx
@@ -0,0 +1,50 @@
+
+import { useState } from "react";
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+} from "@chakra-ui/react";
+
+const Home = () => {
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setFile(event.target.files[0]);
+    }
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Do something with the selected file
+    console.log(file);
+  };
+
+  return (
+    <Box p={4}>
+      <Center>
+        <Heading as="h1" size="xl">
+          Welcome to my app!
+        </Heading>
+      </Center>
+      <Box mt={8}>
+        <form onSubmit={handleSubmit}>
+          <FormControl>
+            <FormLabel>Select a file</FormLabel>
+            <Input type="file" onChange={handleFileChange} />
+          </FormControl>
+          <Button type="submit" mt={4} disabled={!file}>
+            Submit
+          </Button>
+        </form>
+      </Box>
+    </Box>
+  );
+};
+
+export default Home;
