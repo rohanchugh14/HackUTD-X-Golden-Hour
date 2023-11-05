ed0e3975741afc40c6d32b72c23c50c534ecc84d
Rohan Chugh <rohanchugh14@gmail.com>
2023-11-05 13:45:11 -0600

    added img

--- frontend/src/Components/Home/index.tsx
+++ frontend/src/Components/Home/index.tsx
@@ -4,8 +4,14 @@ const Home = () => {
   return (
     <>
       <Heading as="h1" size="2xl" textAlign="center" m={5} fontFamily={"Inter"}>
-        Data Leak Detection
+        Methane Leak Detection
       </Heading>
+      <Stack direction="row" borderRadius={"xl"} m={3} justifyContent={"center"} alignItems={"center"}>
+        <Stack borderRadius="xl" m={3}>
+          {" "}
+          <Image src="/leak.png" />
+        </Stack>
+      </Stack>
       <Stack
         spacing={4}
         p="1rem"
@@ -13,15 +19,15 @@ const Home = () => {
         justifyContent="space-between"
         m={5}
       >
-        <Stack width="33%" borderRadius="xl" m={3}>
+        <Stack borderRadius="xl" m={3}>
           {" "}
           <Image src="/Data_Leak_1.png" />{" "}
         </Stack>
-        <Stack width="33%" borderRadius="xl" m={3}>
+        <Stack borderRadius="xl" m={3}>
           {" "}
           <Image src="/Data_Leak_2.png" />{" "}
         </Stack>
-        <Stack width="33%" borderRadius="xl" m={3}>
+        <Stack borderRadius="xl" m={3}>
           {" "}
           <Image src="/Data_Leak_3.png" />{" "}
         </Stack>
